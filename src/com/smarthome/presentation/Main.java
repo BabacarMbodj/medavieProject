f1c2a00301adcc4649759bb2d6e141ef69745221
admin <admin@admins-MacBook-Pro.local>
2025-04-06 00:33:15 +0000

    creating instances of the different appliances and testing the turnOn/turnOff methods

--- src/com/smarthome/presentation/Main.java
+++ src/com/smarthome/presentation/Main.java
@@ -1,5 +1,8 @@
 package com.smarthome.presentation;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.smarthome.domain.Appliance;
 import com.smarthome.infrastructure.devices.Light;
 import com.smarthome.infrastructure.devices.Fan;
@@ -9,6 +12,22 @@ import com.smarthome.infrastructure.devices.AirConditioner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Appliance light = new Light();
+        Appliance fan = new Fan();
+        Appliance ac = new AirConditioner();
+
+        List<Appliance> appliances = Arrays.asList(light, fan, ac);
+
+        // Testing to turn On then Off each appliance
+
+        appliances.forEach(appliance -> 
+        {
+            appliance.turnOn();
+            appliance.turnOff();
+        }
+    );
+
        
     }
 }
